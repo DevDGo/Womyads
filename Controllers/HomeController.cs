1fe20ab2a0f11785fefc60d0b0c2faf95b0e95f6
DGo <dichu32@hotmail.com>
2012-12-09 09:20:01 -0500

    Carga 2
    Carga 2 Codigo.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using Womyads.Models;
 
 namespace Womyads.Controllers
 {
     public class HomeController : Controller
     {
+        
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "";
 
             return View();
         }
@@ -19,5 +23,27 @@ namespace Womyads.Controllers
         {
             return View();
         }
-    }
+        public ActionResult Contact()
+        {
+            return View(new EnvMail());
+        }
+        [HttpPost] 
+        public ActionResult Contact(EnvMail envmail)
+        {
+         if (!ModelState.IsValid)
+            {
+                return View(envmail);
+            }
+ 
+            var contact = new EnvMail
+            {
+                From = envmail.From,
+                Subject = envmail.Subject,
+                Message = envmail.Message
+            };
+ 
+            new Email().Send(contact);
+            return RedirectToAction("Index");
+        }
+     }
 }
---
+++ Models/Forms.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Configuration;
+using System.Net.Mail;
+using Womyads.Models;
+
+
+namespace Womyads.Models
+{
+    public class EnvMail
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string From { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+    public class Email
+    {
+        public void Send(EnvMail contact)
+        {
+            MailMessage mail = new MailMessage(
+                contact.From,
+                ConfigurationManager.AppSettings[ContactToEmail],
+                contact.Subject,
+                contact.Message);
+            new SmtpClient().Send(mail);
+        }
+    }
+}
+ 
